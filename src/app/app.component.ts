3213a18661af5d5976d5ccd3300d52b60de54da6
Karthikeyaan05 <karthikeyaan05@gmail.com>
2022-06-17 22:36:06 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
   name = 'Angular ' + VERSION.major;
+  dataList: any = [];
+  clickedRowIndex: number;
+
+  ngOnInit(): void {
+    this.http
+      .get<any>('https://jsonplaceholder.typicode.com/posts', {})
+      .subscribe((response: any) => {
+        console.log(response);
+        this.dataList = response;
+      });
+  }
+
+  onEditRow(index) {
+    this.clickedRowIndex = index;
+  }
+
+  onDeleteRow(index) {
+    this.dataList.forEach((value, key) => {
+      if (index == key) {
+        console.log(index, key);
+        this.dataList.splice(index, 1);
+      }
+    });
+  }
 }
